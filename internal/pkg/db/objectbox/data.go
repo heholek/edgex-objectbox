dc14770f92915a38efffa418b8968bd8c962e1b3
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-01-15 14:24:46 +0100

    event update, delete, queries

--- internal/pkg/db/objectbox/data.go
+++ internal/pkg/db/objectbox/data.go
@@ -1,6 +1,7 @@
 package objectbox
 
 // implements core-data service contract
+// TODO queries are not "async-put safe", i. e. there might be changes that have not been written
 
 import (
 	"github.com/edgexfoundry/edgex-go/internal/pkg/db"
@@ -12,14 +13,17 @@ import (
 //region Queries
 type coreDataQueries struct {
 	events struct {
-		device eventQuery
+		device    eventQuery
+		createdB  eventQuery
+		createdLT eventQuery
+		pushedGT  eventQuery
 	}
 	readings struct {
 		device        readingQuery
 		deviceAndName readingQuery
 		name          readingQuery
 		names         readingQuery
-		created       readingQuery
+		createdB      readingQuery
 	}
 }
 
@@ -38,11 +42,29 @@ type readingQuery struct {
 func (client *ObjectBoxClient) initCoreData() error {
 	var err error
 
+	//region Events
 	if err == nil {
 		client.queries.events.device.EventQuery, err =
 			client.eventBox.QueryOrError(obx.Event_.Device.Equals("", true))
 	}
 
+	if err == nil {
+		client.queries.events.createdB.EventQuery, err =
+			client.eventBox.QueryOrError(obx.Event_.Created.Between(0, 0))
+	}
+
+	if err == nil {
+		client.queries.events.createdLT.EventQuery, err =
+			client.eventBox.QueryOrError(obx.Event_.Created.LessThan(0))
+	}
+
+	if err == nil {
+		client.queries.events.pushedGT.EventQuery, err =
+			client.eventBox.QueryOrError(obx.Event_.Pushed.GreaterThan(0))
+	}
+	//endregion
+
+	//region Readings
 	if err == nil {
 		client.queries.readings.device.ReadingQuery, err =
 			client.readingBox.QueryOrError(obx.Reading_.Device.Equals("", true))
@@ -53,7 +75,6 @@ func (client *ObjectBoxClient) initCoreData() error {
 			client.readingBox.QueryOrError(obx.Reading_.Device.Equals("", true), obx.Reading_.Name.Equals("", true))
 	}
 
-	//region Readings
 	if err == nil {
 		client.queries.readings.name.ReadingQuery, err =
 			client.readingBox.QueryOrError(obx.Reading_.Name.Equals("", true))
@@ -65,7 +86,7 @@ func (client *ObjectBoxClient) initCoreData() error {
 	}
 
 	if err == nil {
-		client.queries.readings.created.ReadingQuery, err =
+		client.queries.readings.createdB.ReadingQuery, err =
 			client.readingBox.QueryOrError(obx.Reading_.Created.Between(0, 0))
 	}
 	//endregion
@@ -86,6 +107,8 @@ func (client *ObjectBoxClient) AddEvent(event contract.Event) (objectId string, 
 		event.Created = db.MakeTimestamp()
 	}
 
+	// TODO readings
+
 	var id uint64
 	if client.asyncPut {
 		id, err = client.eventBox.PutAsync(&event)
@@ -101,20 +124,40 @@ func (client *ObjectBoxClient) AddEvent(event contract.Event) (objectId string, 
 }
 
 func (client *ObjectBoxClient) UpdateEvent(e contract.Event) error {
-	panic(notImplemented())
+	e.Modified = db.MakeTimestamp()
+
+	// check whether it exists, otherwise this function must fail
+	if object, err := client.eventById(e.ID); err != nil {
+		return err
+	} else if object == nil {
+		return db.ErrNotFound
+	}
+
+	var err error
+	if client.asyncPut {
+		_, err = client.eventBox.PutAsync(&e)
+	} else {
+		_, err = client.eventBox.Put(&e)
+	}
+
+	return err
 }
 
 func (client *ObjectBoxClient) EventById(idString string) (contract.Event, error) {
-	id, err := idStringToObx(idString)
-	if err != nil {
-		return contract.Event{}, err
-	}
-
-	object, err := client.eventBoxForReads().Get(id)
+	object, err := client.eventById(idString)
 	if object == nil || err != nil {
 		return contract.Event{}, err
 	}
 	return *object, nil
+}
+
+func (client *ObjectBoxClient) eventById(idString string) (*contract.Event, error) {
+	id, err := idStringToObx(idString)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.eventBoxForReads().Get(id)
 }
 
 func (client *ObjectBoxClient) EventCount() (count int, err error) {
@@ -125,41 +168,84 @@ func (client *ObjectBoxClient) EventCount() (count int, err error) {
 	return
 }
 
-func (ObjectBoxClient) EventCountByDeviceId(id string) (int, error) {
-	panic(notImplemented())
-}
-
-func (ObjectBoxClient) DeleteEventById(id string) error {
-	panic(notImplemented())
-}
-
-func (ObjectBoxClient) EventsForDeviceLimit(id string, limit int) ([]contract.Event, error) {
-	panic(notImplemented())
-}
-
-func (client *ObjectBoxClient) EventsForDevice(deviceId string) ([]contract.Event, error) {
+func (client *ObjectBoxClient) EventCountByDeviceId(id string) (int, error) {
 	var query = &client.queries.events.device
 
 	query.Lock()
 	defer query.Unlock()
 
-	if err := query.SetStringParams(obx.Event_.Device, deviceId); err != nil {
+	if err := query.SetStringParams(obx.Event_.Device, id); err != nil {
+		return 0, err
+	}
+
+	count, err := query.Count()
+	return int(count), err
+}
+
+func (client *ObjectBoxClient) DeleteEventById(idString string) error {
+	// TODO maybe this requires a check whether the item exists
+
+	id, err := idStringToObx(idString)
+	if err != nil {
+		return err
+	}
+
+	client.objectBox.AwaitAsyncCompletion()
+	return client.eventBox.Box.Remove(id)
+}
+
+func (client *ObjectBoxClient) EventsForDeviceLimit(id string, limit int) ([]contract.Event, error) {
+	var query = &client.queries.events.device
+
+	query.Lock()
+	defer query.Unlock()
+
+	if err := query.SetStringParams(obx.Event_.Device, id); err != nil {
+		return nil, err
+	}
+
+	return query.Limit(uint64(limit)).Find()
+}
+
+func (client *ObjectBoxClient) EventsForDevice(id string) ([]contract.Event, error) {
+	return client.EventsForDeviceLimit(id, 0)
+}
+
+func (client *ObjectBoxClient) EventsByCreationTime(start, end int64, limit int) ([]contract.Event, error) {
+	var query = &client.queries.events.createdB
+
+	query.Lock()
+	defer query.Unlock()
+
+	if err := query.SetInt64Params(obx.Event_.Created, start, end); err != nil {
+		return nil, err
+	}
+
+	return query.Limit(uint64(limit)).Find()
+}
+
+func (client *ObjectBoxClient) EventsOlderThanAge(age int64) ([]contract.Event, error) {
+	var time = (db.MakeTimestamp()) - age
+
+	var query = &client.queries.events.createdLT
+
+	query.Lock()
+	defer query.Unlock()
+
+	if err := query.SetInt64Params(obx.Event_.Created, time); err != nil {
 		return nil, err
 	}
 
 	return query.Find()
 }
 
-func (ObjectBoxClient) EventsByCreationTime(startTime, endTime int64, limit int) ([]contract.Event, error) {
-	panic(notImplemented())
-}
+func (client *ObjectBoxClient) EventsPushed() ([]contract.Event, error) {
+	var query = &client.queries.events.pushedGT
 
-func (ObjectBoxClient) EventsOlderThanAge(age int64) ([]contract.Event, error) {
-	panic(notImplemented())
-}
+	query.Lock()
+	defer query.Unlock()
 
-func (ObjectBoxClient) EventsPushed() ([]contract.Event, error) {
-	panic(notImplemented())
+	return query.Find()
 }
 
 func (client *ObjectBoxClient) ScrubAllEvents() (err error) {
@@ -226,11 +312,7 @@ func (client *ObjectBoxClient) readingById(idString string) (*contract.Reading, 
 		return nil, err
 	}
 
-	object, err := client.readingBoxForReads().Get(id)
-	if object == nil || err != nil {
-		return nil, err
-	}
-	return object, nil
+	return client.readingBoxForReads().Get(id)
 }
 
 func (client *ObjectBoxClient) ReadingCount() (count int, err error) {
@@ -239,7 +321,9 @@ func (client *ObjectBoxClient) ReadingCount() (count int, err error) {
 	return
 }
 
-func (client ObjectBoxClient) DeleteReadingById(idString string) error {
+func (client *ObjectBoxClient) DeleteReadingById(idString string) error {
+	// TODO maybe this requires a check whether the item exists
+
 	id, err := idStringToObx(idString)
 	if err != nil {
 		return err
@@ -289,7 +373,7 @@ func (client *ObjectBoxClient) ReadingsByValueDescriptorNames(names []string, li
 }
 
 func (client *ObjectBoxClient) ReadingsByCreationTime(start, end int64, limit int) ([]contract.Reading, error) {
-	var query = &client.queries.readings.created
+	var query = &client.queries.readings.createdB
 
 	query.Lock()
 	defer query.Unlock()

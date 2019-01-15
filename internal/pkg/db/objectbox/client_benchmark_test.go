404bfb0b2407f1e65caac896e95b099047f63b1c
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-01-15 13:14:46 +0100

    benchmark defer mutex.unlock vs no-defer

--- internal/pkg/db/objectbox/client_benchmark_test.go
+++ internal/pkg/db/objectbox/client_benchmark_test.go
@@ -1,11 +1,13 @@
 package objectbox
 
 import (
-	"testing"
-
 	"fmt"
 	"github.com/edgexfoundry/edgex-go/internal/pkg/db"
 	"github.com/edgexfoundry/edgex-go/internal/pkg/db/test"
+	"sync"
+	"testing"
+
+	contract "github.com/edgexfoundry/edgex-go/pkg/models"
 )
 
 func BenchmarkObjectBox(b *testing.B) {
@@ -30,4 +32,32 @@ func TestBenchmarkFixedNObjectBox(t *testing.T) {
 		return
 	}
 	test.BenchmarkDBFixedN(client, true)
+}
+
+var mutex sync.Mutex
+
+func doNoDefer() (result []contract.Event, err error) {
+	mutex.Lock()
+
+	result = []contract.Event{}
+
+	mutex.Unlock()
+	return
+}
+func doDefer() ([]contract.Event, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	return []contract.Event{}, nil
+}
+
+func BenchmarkDeferYes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		doDefer()
+	}
+}
+func BenchmarkDeferNo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		doNoDefer()
+	}
 }

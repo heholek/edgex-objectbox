a6fc75b58ac72e083c4f51a8fe56972ab33f7f27
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-11-11 18:29:07 +0100

    script to create multi-arch docker manifests

---
+++ build/docker-multiarch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Note: to delete a manifest, remove it manually from ~/.docker/manifests/
+
+version=$(cat ./VERSION)
+
+declare -a archs=("x86_64" "armv7l")
+
+# exact tag for the current architecture & version - used to find the list of images
+thisArchTag=$(uname -m)-$version
+
+# get the list of image names (without tags) to combine
+images=$(docker image ls | grep "objectboxio/edge-" | grep "$thisArchTag" | cut -d' ' -f1)
+
+declare -a creates=()
+declare -a pushes=()
+for image in ${images[*]}; do
+    target="$image:$version"
+
+    manifestCreate="docker manifest create $target "
+    for arch in ${archs[*]}; do
+        manifestCreate="$manifestCreate $image:$arch-$version"
+    done
+    creates+=("$manifestCreate")
+    pushes+=("docker manifest push $target")
+done
+
+printf '%s\n' "${creates[@]}"
+printf '%s\n' "${pushes[@]}"

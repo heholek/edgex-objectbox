ee5f484692099a9051d3b4727be09e14e6e308f1
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-11-12 16:46:47 +0100

    add a script to push built images to docker hub

---
+++ build/docker-push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Note: to delete a manifest, remove it manually from ~/.docker/manifests/
+
+version=$(cat ./VERSION)
+
+images=$(docker images "objectboxio/edgex-*:*$version" --format "{{.Repository}}:{{.Tag}}")
+
+echo "$images"
+
+read -p "Would you like to push all images? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "$images" | xargs -n1 docker push
+fi
